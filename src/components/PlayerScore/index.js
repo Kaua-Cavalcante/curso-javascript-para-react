e219f228ab503acf7a69176dae9aab8b8387a706
Kauã Cavalcante <kaua.ferreiracavalcante@gmail.com>
2022-01-23 16:25:39 -0300

    :sparkles: Create PlayerScore component

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,13 @@
+import  './style.css';
+
+function PlayerScore() {
+    return `
+        <div class="player-score">
+            <div class="score"></div>
+            <div class="score"></div>
+            <div class="score"></div>
+        </div>
+    `;
+}
+
+export default PlayerScore;
