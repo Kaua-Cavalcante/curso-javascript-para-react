d09b3340b0bd1301b40f29a220fa3092e01e1130
Kauã Cavalcante <kaua.ferreiracavalcante@gmail.com>
2022-01-24 00:14:17 -0300

    :construction: Create PlayerTurn component without CSS

---
+++ src/components/PlayerTurn/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function PlayerTurn() {
+    return `
+        <div class="player-turn">
+            <img src="images/arrow-down.svg" alt="Seta para baixo">
+        </div>
+    `;
+}
+
+export default PlayerTurn;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,14 @@
 import "./style.css";
+import PlayerTurn from "../../components/PlayerTurn";
 import PlayerName from "../../components/PlayerName";
 import PlayerScore from "../../components/PlayerScore";
 import PlayerVersus from "../../components/PlayerVersus";
 
 function ScoreBoard() {
   return `
+  ${PlayerTurn()}
         <header class="score-board">  
+        
             <div class="score-container">
                 ${PlayerName("Player1")} ${PlayerScore()}
             </div>
